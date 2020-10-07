689943f01ff258181f3b5101c3f11b39414be15c
Jaume <jasato89@gmail.com>
2020-10-07 19:02:29 +0200

    Update CSS HTML

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Boletin Variables</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <h1>Boletin variables</h1>
+
+    <div>
+        <ul>
+            <li>Escribe un programa que muestre tu nombre por pantalla, nombre del ciclo formativo que estás
+                realizando y curso.</li>
+            <li><a href="">Solución</a></li>
+            <li>Modifica el programa anterior para que muestre tu dirección, nombre de las asignaturas
+                del ciclo.El texto debe aparecer en una línea diferente.</li>
+            <li><a href="">Solución</a></li>
+            <li>Escribe un programa que muestre tu horario de clase mediante una tabla, el recreo debe de aparecer.
+                Dale formato y estilos a la tabla.</li>
+            <li><a href="">Solución</a></li>
+            <li>Crea la variable $nombre y asígnale tu nombre completo, crea la variable ciclo y asiga el
+                nombre del ciclo. Muestralo por pantalla como el ejercicio 1.</li>
+            <li><a href="">Solución</a></li>
+            <li>Crea un conversor de moneda, de dólares a libras Australianos. Haz uso de variables.</li>
+            <li><a href="">Solución</a></li>
+            <li>Pinta una pirámide invertida y hueca con asteriscos.</li>
+            <li><a href="">Solución</a></li>
+
+        </ul>
+    </div>
+
+</body>
+
+</html>
