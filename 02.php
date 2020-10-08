4bcfcb27d963319d4d349df59ec6048d1adc14ea
Jaume <jasato89@gmail.com>
2020-10-08 12:22:18 +0200

    Updated templates and exercise 1 finished

--- 02.php
+++ 02.php
@@ -1,19 +1,33 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Soluciones</title>
+    <title>Ejercicio 1</title>
+    <link rel="stylesheet" href="style.css">
+
 </head>
 
-    <h1>Soluciones de los ejercicios</h1>
+<h1>Soluciones de los ejercicios</h1>
+<h2>Ejercicio 1</h2>
 
-    <div>
-
-
+<div>
+    <div class="innerDiv">
+        <p><b><?php echo("Jaume Sánchez Torres"); ?></b></p>
+        <p><?php echo("Desarrollo de aplicaciones multiplataforma"); ?></p>
     </div>
 
+    <div class="innerDiv">
+        <h3>Código fuente:</h3>
+        
+    </div>
+    <img src="01.png" alt="">
+
+</div>
+
 <body>
-    
+
 </body>
+
 </html>
---
+++ test.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<script src="https://gist.github.com/jasato89/9dc4f75837b2c2fb92dfe65628be75c1.js"></script>
+</body>
+</html>
